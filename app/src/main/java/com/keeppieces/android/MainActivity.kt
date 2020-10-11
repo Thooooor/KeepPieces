2d7fcc45f68ce306e9f46556502ddaf79ff64fc1
Thooooor <46197373+Thooooor@users.noreply.github.com>
2020-10-11 13:19:36 +0800

    Add top tab

--- app/src/main/java/com/keeppieces/android/MainActivity.kt
+++ app/src/main/java/com/keeppieces/android/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.keeppieces.android
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.observe
 import com.keeppieces.android.logic.data.AppDatabase
-import com.keeppieces.android.ui.bill.BillActivity
-import com.keeppieces.android.ui.daily.DailyActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
 
 @Suppress("COMPATIBILITY_WARNING")
 class MainActivity : AppCompatActivity() {
@@ -19,7 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState == null) runEnterAnimation()
+        setUpViewPager()
+
         val billDao = AppDatabase.getDatabase(this).billDao()
+        /*
         addBillButton.setOnClickListener {
             val intent = Intent(this, BillActivity::class.java)
             startActivity(intent)
@@ -46,6 +45,28 @@ class MainActivity : AppCompatActivity() {
         dailyButton.setOnClickListener {
             val intent = Intent(this, DailyActivity::class.java)
             startActivity(intent)
+        }*/
+    }
+
+    private fun setUpViewPager() {
+        val tabs = generateTabs()
+        view_pager.adapter = MainPagerAdapter(supportFragmentManager, tabs)
+        view_pager.offscreenPageLimit = 0
+        tab_layout.setUpWithViewPager(view_pager, false)
+
+        view_pager.setCurrentItem(0, true)
+    }
+
+    private fun runEnterAnimation() {
+        tab_layout.post {
+            tab_layout.translationY -= tab_layout.height.toFloat()
+            tab_layout.alpha = 0f
+            tab_layout.animate()
+                .translationY(0f)
+                .setInterpolator(AccelerateDecelerateInterpolator())
+                .alpha(1f)
+                .setDuration(300)
+                .start()
         }
     }
 
