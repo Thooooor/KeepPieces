4633edc67c45c6ebc5afd04f67c221f683e0134d
Thooooor <46197373+Thooooor@users.noreply.github.com>
2020-10-15 00:12:07 +0800

    Create Detail Activity

---
+++ app/src/main/java/com/keeppieces/android/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.keeppieces.android.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.keeppieces.android.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
