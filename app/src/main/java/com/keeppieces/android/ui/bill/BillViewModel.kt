5520c527b1a91f6d84c50a9e36dc8769ff654ba8
2439 <45897549+2439@users.noreply.github.com>
2020-10-14 23:25:22 +0800

    update AppDatabase.kt update Dao for onConflict update SecondaryCategory.kt for some bugs success add bill in BillActivity.kt button in TopTab used to startActivity(BillActivity)

--- app/src/main/java/com/keeppieces/android/ui/bill/BillViewModel.kt
+++ app/src/main/java/com/keeppieces/android/ui/bill/BillViewModel.kt
@@ -2,16 +2,26 @@ package com.keeppieces.android.ui.bill
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.keeppieces.android.logic.data.Bill
-import com.keeppieces.android.logic.data.BillRepository
+import com.keeppieces.android.logic.data.*
 
 class BillViewModel() : ViewModel() {
     private val billLiveData = MutableLiveData<Bill>()
 
-    init {
+    var bill = Bill(
+        System.currentTimeMillis().toString(),
+        0.00,
+        "现金",
+        "自己",
+        "三餐",
+        "早餐",
+        "支出")
 
-    }
     fun addBill(bill: Bill) {
-        billLiveData.value = bill
+        AccountRepository().createAccount(Account(bill.account,bill.amount))
+        MemberRepository().createMember(Member(bill.member))
+        PrimaryCategoryRepository().createPrimaryCategory(PrimaryCategory(bill.primaryCategory))
+        SecondaryCategoryRepository().createSecondaryCategory(SecondaryCategory(bill.secondaryCategory, bill.primaryCategory))
+        TypeRepository().createType(Type(bill.type))
+        BillRepository().insertBill(bill)
     }
 }
