c8547d29e2fbad7ed3e67a5d73f39b2d29720dcc
wwz <792602520@qq.com>
2020-10-16 19:14:01 +0800

    add getAllBill function add getPeriodBill function

--- app/src/main/java/com/keeppieces/android/logic/data/BillDao.kt
+++ app/src/main/java/com/keeppieces/android/logic/data/BillDao.kt
@@ -19,4 +19,13 @@ interface BillDao {
 
     @Query("SELECT * FROM bill ORDER BY amount")
     fun loadAllBillByAmount(): LiveData<List<Bill>>
+
+//    @Query("SELECT * FROM bill WHERE date LIKE :year+'-'+:month+'-%' ORDER BY bill_id")
+//    fun getOneMonthBill(year:String, month: String): LiveData<List<Bill>>
+
+    @Query("SELECT * FROM bill")
+    fun getAllBill(): LiveData<List<Bill>>
+
+    @Query("SELECT * FROM bill where DATETIME(date) BETWEEN DATETIME(:startDate) AND DATETIME(:endDate)")
+    fun getPeriodBill(startDate:String,endDate:String):LiveData<List<Bill>>
 }
