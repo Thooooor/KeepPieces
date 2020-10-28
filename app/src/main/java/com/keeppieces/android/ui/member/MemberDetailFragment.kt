024821897cdcbb3449b5be93e6511578dc03f334
Thooooor <46197373+Thooooor@users.noreply.github.com>
2020-10-29 00:40:40 +0800

    Remove Swipe Bill

--- app/src/main/java/com/keeppieces/android/ui/member/MemberDetailFragment.kt
+++ app/src/main/java/com/keeppieces/android/ui/member/MemberDetailFragment.kt
@@ -1,16 +1,12 @@
 package com.keeppieces.android.ui.member
 
-import android.graphics.Canvas
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.keeppieces.android.R
 import com.keeppieces.android.extension.getItemDecoration
 import com.keeppieces.android.logic.data.Bill
@@ -34,33 +30,6 @@ class MemberDetailFragment(var startDate: String, var endDate: String, var membe
         setUpRecyclerView()
     }
 
-    private val myCallback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-        override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-        ): Boolean = false
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            val position = viewHolder.adapterPosition
-
-            Log.d("Detail", position.toString())
-        }
-
-        override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-        ) {
-            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-            c.clipRect(0f, viewHolder.itemView.top.toFloat(), dX, viewHolder.itemView.bottom.toFloat())
-        }
-    }
-
     private fun setUpRecyclerView() {
         viewModel.getMemberPeriodList(startDate, endDate, member).observe(viewLifecycleOwner) {billList ->
             val bills = if (billList.isEmpty()) listOf() else getBillsFilter(billList)
@@ -70,12 +39,9 @@ class MemberDetailFragment(var startDate: String, var endDate: String, var membe
                 setHasFixedSize(true)
                 addItemDecoration(getItemDecoration())
                 adapter = DetailAdapter(bills)
-                val myHelper = ItemTouchHelper(myCallback)
-                myHelper.attachToRecyclerView(this)
             }
         }
     }
-
 
     private fun getBillsFilter(billList: List<Bill>) : List<Bill> {
         val billsFilter = mutableListOf<Bill>()
