979513c3fef09b83607576392a0e9244fa074296
2439 <45897549+2439@users.noreply.github.com>
2020-10-29 03:27:33 +0800

    OverviewFragment.kt 账户跳转和数据

--- app/src/main/java/com/keeppieces/android/ui/overview/homepage_card_adapter/AccountSummaryCardAdapter.kt
+++ app/src/main/java/com/keeppieces/android/ui/overview/homepage_card_adapter/AccountSummaryCardAdapter.kt
@@ -1,16 +1,23 @@
 package com.keeppieces.android.ui.overview.homepage_card_adapter
 
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.keeppieces.android.R
 import com.keeppieces.android.extension.toMoneyFormatted
 import com.keeppieces.android.logic.data.DailyAccount
+import com.keeppieces.android.ui.account.AccountActivity
 import com.keeppieces.line_indicator.VerticalBar
 import com.keeppieces.line_indicator.VerticalBarData
+import java.time.LocalDate
+import java.time.chrono.IsoChronology
+import java.util.*
 
 class AccountSummaryCardAdapter(private val content:Context, private val accountSummaryList: List<DailyAccount>):
     RecyclerView.Adapter<AccountSummaryCardAdapter.ViewHolder>(){
@@ -21,11 +28,34 @@ class AccountSummaryCardAdapter(private val content:Context, private val account
         val moneySymbol:TextView = view.findViewById(R.id.money_symbol_in_item)
         val moneyAmount:TextView = view.findViewById(R.id.money_amount_in_item)
         val bar:VerticalBar = view.findViewById(R.id.item_vertical_bar)
+        val accountMoreInformationButton: ConstraintLayout = view.findViewById(R.id.more_information)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(content).inflate(R.layout.item_summary_card,parent,false)
-        return ViewHolder(view)
+        val viewHolder = ViewHolder(view)
+        val today = Calendar.getInstance()
+        val year = today.get(Calendar.YEAR)
+        val month = today.get(Calendar.MONTH) + 1
+        val lastDay = when(month) {
+            2 -> if (IsoChronology.INSTANCE.isLeapYear(year.toLong())) 29 else 28
+            4 -> 30
+            6 -> 30
+            9 -> 30
+            11 -> 30
+            else -> 31
+        }
+
+        val startDate = LocalDate.of(year, month, 1).toString()
+        val endDate = LocalDate.of(year, month, lastDay).toString()
+
+        viewHolder.accountMoreInformationButton.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            val account = accountSummaryList[position].account
+            AccountActivity.start(content, startDate, endDate, account)
+        }
+        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
